d9413524a928bcf841a8eaea5c85b0a4f411a4ad
Stephane Fuentes <stephane.fuentes07@gmail.com>
2025-07-26 18:24:08 +0200

    derniere modif rushh

---
+++ rushhh/rush04.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nourra.agoudjil <nourra.agoudjil@learner.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 17:17:36 by nourra.agoudj     #+#    #+#             */
+/*   Updated: 2025/07/26 17:41:17 by nourra.agoudj    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print_line(char start, char fill, char end, int lc);
+
+void	ft_print_line(char start, char fill, char end, int lc)
+{
+	int	i;
+
+	i = 0;
+	ft_putchar(start);
+	while (i < (lc - 2))
+	{
+		ft_putchar(fill);
+		i++;
+	}
+	if (lc > 1)
+	{
+		ft_putchar(end);
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 0;
+	if (x > 0 && y > 0)
+	{
+		ft_print_line('A', 'B', 'C', x);
+	}
+	if (y > 2)
+	{
+		while (i < (y - 2))
+		{
+			ft_print_line('B', ' ', 'B', x);
+			i++;
+		}
+	}
+	if (y > 1)
+	{
+		ft_print_line('C', 'B', 'A', x);
+	}
+}
