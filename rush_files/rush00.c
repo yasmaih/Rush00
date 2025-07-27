441c23eef0d9891ce6aa61cb5d17ec2c6965c115
Noura Agoudjil <noura.agoudjil@hotmail.fr>
2025-07-27 10:19:06 +0200

    Ajout d'une gestion d'erreur

--- rush_files/rush00.c
+++ rush_files/rush00.c
@@ -6,7 +6,7 @@
 /*   By: nourra.agoudjil <nourra.agoudjil@learner.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 19:44:18 by nourra.agoudj     #+#    #+#             */
-/*   Updated: 2025/07/26 20:18:27 by nourra.agoudj    ###   ########.fr       */
+/*   Updated: 2025/07/27 10:11:16 by nourra.agoudj    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,11 @@ void	rush(int x, int y)
 	int	i;
 
 	i = 0;
+	if(((x < 0)||y < 0)||((x > 2147483647)||(y > 2147483647)))
+	{
+		write(1, "Merci de recommencer uniquement avec des nombres entiers positifs, compris entre 0 et 2 147 483 648", 99);
+		write(1, "\n", 1);
+	}	
 	if (x > 0 && y > 0)
 	{
 		ft_print_line('o', '-', 'o', x);
